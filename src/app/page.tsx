d66ed06f7ac4b07198ee93af6fc6b4f3fbdb4944
Adeena <160261894+Adeenashaikh9206@users.noreply.github.com>
2024-10-31 00:17:37 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Header from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Icons from "./components/icons";
+import Flower from "./components/flower";
+import Contact from "./components/contact";
+import Foter from "./components/footer";
+
+export default function home () {
+  return(
+    <div>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Icons/>
+      <Flower/>
+      <Contact/>
+      <Foter/>
+      
+    </div>
+  )
+}
